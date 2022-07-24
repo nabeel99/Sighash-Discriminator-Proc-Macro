10f9e080fa678ade1fd6bcc6619857965e7a98ac
nabeel99 <ac3r_nabeel@live.com>
2022-07-24 19:03:34 +0500

    removed the need to convert slice to iterator, as its redundant , added reasoning

--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,22 @@ use sighash::sig_hash;
 
 #[proc_macro_attribute]
 pub fn get_fn_discriminator(_metadata: TokenStream, body: TokenStream) -> TokenStream {
+    //syn exposes a parse trait to use the type in parse_macro_input
+    //parse_macro_input takes 2 arguments, a identifier assuming its a token stream
+    //and a type to call syn parse with,
+    //here we pass token stream as first argument
+    //and item fn as the type we want the parse macro to parse out.
     let input = parse_macro_input!(body as ItemFn);
-
+    // Repetition is done using #(...)* or #(...),* again similar to macro_rules!.
+    // This iterates through the elements of any variable interpolated within the repetition
+    //  and inserts a copy of the repetition body for each one.
+    //  The variables in an interpolation may be a Vec, slice, BTreeSet, or any Iterator.
+    //no need to convert sig_hash_arr into an iterator
     let fn_name: &str = &input.sig.ident.to_string();
     let name_str = format!("{}_SIGHASH", fn_name.to_ascii_uppercase());
     let new_name = Ident::new(&name_str[..], Span::call_site());
-    let sig_hash = sig_hash(fn_name).into_iter();
-    let discrim = quote! {pub const #new_name : [u8;8] = [#(#sig_hash),*];
+    let sig_hash_arr = sig_hash(fn_name);
+    let discrim = quote! {pub const #new_name : [u8;8] = [#(#sig_hash_arr),*];
     #input}
     .into();
     discrim
